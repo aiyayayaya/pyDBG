c6d2b84e516c5b2626d4400ed2998c6d2d1ac786
aiyayayaya <harmonyyap@gmail.com>
2019-06-24 15:16:09 +0100

    added register inspector

--- my_debugger.py
+++ my_debugger.py
@@ -8,6 +8,41 @@ class debugger():
     def __init__(self):
         pass
 
+    def open_thread(self, thread_id):
+        h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None, thread_id)
+        if h_thread is not None:
+            return h_thread
+        else:
+            print("[g] Could not obtain a valid thread handle.")
+
+    def enumerate_threads(self):
+        thread_entry = THREADENTRY32()
+        thread_list = []
+        sapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+        if snapshot is not None:
+            #set the size of the struct or the call fail
+            thread_entry.dwSize = sizeof(thread_entry)
+            success = kernel32.Thread32First(snapshot, byref(thread_entry))
+        while success:
+            if thread_entry.th32OwnerProcessID == self.pid:
+                thread_list.append(thread_entry.th32ThreadID)
+                success =h kernel32.Thread32Next(snapshot, byref(thread_entry)
+                        kernel32.CloseHandle(snapshot)
+                        return thread_list
+        else:
+        return False
+
+    def get_thread_context(self, thread_id):
+        context = CONTEXT()
+        cnotext.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+
+        # Obtain a handle to the thread
+        h_thread = self.open.thread(thread_id)
+        if kernel32.GetThreadContext(h_thread, byref(context)):
+            kernel32.CloseHandle(h_thread)
+        else:
+            return False
+
     def load(self, path_to_exe):
 
         # dwCreation flag determines how to create the process
@@ -38,8 +73,8 @@ class debugger():
                 None,
                 byref(startupinfo),
                 byref(process_information)):
-            print "[*] We have successfully launched the process!"
-            print "[*] PID: %d" % process_information.dwProcessId
+            print("[*] We have successfully launched the process!")
+            print("[*] PID: %d" % process_information.dwProcessId)
         else:
-            print "[*] Error: 0x%08x." % kernel32.GetLastError()
+            print("[*] Error: 0x%08x." % kernel32.GetLastError())
 
--- my_test.py
+++ my_test.py
@@ -1,5 +1,22 @@
 import my_debugger
 
 debugger = my_debugger.debugger()
+pid = input("Enter the PID of the process to attach to: ")
+debugger.attach(int(pid))
+listing = debugger.enumerate_threads()
 
-debugger.load("C:\\WINDOWS\\system32\\calc.exe")
+# for each thread in the list, grab the value of each registers
+for thread in listing:
+    thread_content = debugger.get_thread_context(thread)
+    # output the contents of registers
+    print("[*] Dumping registers for thread ID: 0x%08x" % thread)
+    print("[] EIP: 0x%08x" % thread_context.Eip)
+    print("[] ESP: 0x%08x" % thread_context.Esp)
+    print("[] EBP: 0x%08x" % thread_context.Ebp)
+    print("[] EAX: 0x%08x" % thread_context.Eax)
+    print("[] EBX: 0x%08x" % thread_context.Ebx)
+    print("[] ECX: 0x%08x" % thread_context.Ecx)
+    print("[] EDX: 0x%08x" % thread_context.Edx)
+    print("[] END DUMP")
+
+debugger.detach()
